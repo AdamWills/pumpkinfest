5faec3527d55b3be7e41f7193a9fafa4841a0518
Adam Wills <heavymetaladam@gmail.com>
2016-08-07 15:47:32 -0400

    Updated date in header

--- templates/content-hero.php
+++ templates/content-hero.php
@@ -1,6 +1,6 @@
 <?php use Roots\Sage\Assets; ?>
 
 <img src="<?php echo Assets\asset_path('images/logo.png'); ?>" alt="">
-<p class="pre">October 16 - 18, 2015</p>
+<p class="pre">October 14 - 16, 2016</p>
 <h1>Waterford Pumpkinfest</h1>
 <p><a class="btn btn-secondary btn-lg" href="https://www.facebook.com/waterfordpumpkinfest">Find us on Facebook</a> <a class="btn btn-secondary btn-lg" href="/schedule/">View the Schedule</a></p>
